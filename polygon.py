3ec277bf6676781f84ea31f4e1c021f1ad37f0bc
Robert <robert.niemiec@smcebi.edu.pl>
2017-02-08 20:28:19 +0100

    created basic area calculator

--- polygon.py
+++ polygon.py
@@ -1,8 +1,21 @@
+#!/usr/bin/env python
 import math
 
-def dist_calc(x,y):
+def dist_calc(x, y):
     x_dist = abs(x[0] - y[0])
     y_dist = abs(x[1] - y[1])
     print "x distance: ", x_dist
     print "y distance: ", y_dist
     return math.sqrt(x_dist**2 + y_dist**2)
+
+
+def poly_area(midpt, vertex_num, vert_to_midpt_dist):
+    area = 0
+    angle = 360.0/vertex_num
+    tri_area = 0.5 * vert_to_midpt_dist**2 * math.sin(math.radians(angle))
+    for i in range(vertex_num):
+        area += tri_area
+    return area
+
+# print dist_calc([0,0],[1,1])
+print poly_area([0,0],3,5)
